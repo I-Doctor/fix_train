13710df56e133aad4b5d4ea6c8abe1a69dd4da31
zhongkai <zhongkai@zktest.eva8.nics.cc>
2020-01-20 17:26:36 +0800

    clean up some useless scripts, and ready to run imgnet 4-bit experiment without quantize bias

--- code/cifar-check-d.sh
+++ code/cifar-check-d.sh
@@ -11,22 +11,17 @@ time=`date +"%Y%m%d_%H-%M-%S"`
 echo "Processing at time: ${time}"
 
 #--------------------mkdir-----------------------#
-output_dir_name="log_quantize_check"
+output_dir_name="log_quantize_check-d"
 output_path="${checkpoint_path}/${output_dir_name}"
 echo "Creating output dir: ${output_path}"
 mkdir -p ${output_path}
 
 #--------------------run python------------------#
-cfg_file="d-0-4bit-linear-glevel4"
-#cfg_file="d-0-4bit-linear-glevel4-resume"
-#cfg_file="d-0-4bit-linear-glevel4-evaluate"
-#cfg_file="0-8bit-linear-glevel2-resume"
-#cfg_file="0-8bit-linear-glevel2-evaluate"
+cfg_file="d-cifar-check"
 cfg_path="../config/cifar10/fix_cfg/"
-python main.py				        \
+python -u main.py			        \
 	${data_root}					\
 	${cfg_path}${cfg_file}.yaml		\
 	${output_path}					\
-    #--gpu 2                         \
 	2>&1 | tee ${output_path}/${cfg_file}.log
 
--- code/imgnet-check.sh
+++ code/imgnet-check.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 #--------------------set global param------------#
-data_root="/home/eva_share/datasets/"
+data_root="/home/eva_share/datasets/ILSVRC2012/"
 current_path=$(pwd)
-checkpoint_path="../checkpoint/fix_experiment/cifar"
+checkpoint_path="../checkpoint/fix_experiment/imgnet"
 
 
 #--------------------get date--------------------#
@@ -17,20 +17,17 @@ echo "Creating output dir: ${output_path}"
 mkdir -p ${output_path}
 
 #--------------------run python------------------#
-#cfg_file="0-4bit-linear-glevel4"
-#cfg_file="00-4bit-linear-glevel4"
-#cfg_file="10-4bit-linear-glevel4"
-#cfg_file="20-4bit-linear-glevel4"
-cfg_file="30-4bit-linear-glevel4"
-#cfg_file="0-4bit-linear-glevel4-resume"
-#cfg_file="0-4bit-linear-glevel4-evaluate"
+cfg_file="d-0-8bit-linear-glevel4"
+#cfg_file="d-0-8bit-linear-glevel4-evaluate"
+#cfg_file="d-0-8bit-linear-glevel4-resume"
+#cfg_file="d-0-4bit-linear-glevel4-resume"
+#cfg_file="d-0-4bit-linear-glevel4-evaluate"
 #cfg_file="0-8bit-linear-glevel2-resume"
 #cfg_file="0-8bit-linear-glevel2-evaluate"
-cfg_path="../config/cifar10/fix_cfg/"
-python main.py				        \
+cfg_path="../config/imgnet/fix_cfg/"
+python -u main.py 	    	        \
 	${data_root}					\
 	${cfg_path}${cfg_file}.yaml		\
 	${output_path}					\
-    --gpu 3                         \
 	2>&1 | tee ${output_path}/${cfg_file}.log
 
