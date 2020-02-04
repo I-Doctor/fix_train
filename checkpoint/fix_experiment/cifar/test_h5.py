274c14ba94f03f3a08ed3f69e9987b095d366c15
I-Doctor <ZK14115@163.com>
2020-02-04 17:17:35 +0800

    add simple magnitude scaling

--- checkpoint/fix_experiment/cifar/test_h5.py
+++ checkpoint/fix_experiment/cifar/test_h5.py
@@ -1,28 +1,34 @@
 import numpy as np
 import h5py
+import os 
+import sys
 
-dir = 'log_quantize_check-d/'
-#dir = 'log_quantize_check/'
+dirname = 'log_quantize_check'
+typename = 'e'
+epoch = 90
+layername = 'layers.0.2.bn2'
+filename = os.path.join(dirname,'checkpoint_'+typename+'_'+str(epoch)+'.h5')
 
-#filename = 'checkpoint_w_0.h5'
-#filename = 'checkpoint_a_0.h5'
-filename = 'checkpoint_e_0.h5'
-#filename = 'checkpoint_g_0.h5'
+if not os.path.exists(filename):
+    print(filename)
+    exit("wrong filename")
 
-#filename = 'checkpoint_w_2.h5'
-#filename = 'checkpoint_a_2.h5'
-#filename = 'checkpoint_e_2.h5'
-#filename = 'checkpoint_g_2.h5'
-
-f = h5py.File(dir+filename)
+f = h5py.File(filename,'r')
 keys = f.keys()
 for layer_name in keys:
-    layer_data = f[layer_name]
-    layer_shape = layer_data.shape
-    if len(layer_shape) == 4:
+    layer_data = f[layer_name][:]
+    layer_name = layer_name[0:-6]
+    if layer_name == layername:
         print(layer_name)
-        print('  layer_shape',layer_data.shape)
-        print('  layer_value',layer_data[0,1,0:2,0:2])
-    #print(layer_name)
-    #print('  layer_shape',layer_data.shape)
-
+        shape = layer_data.shape
+        print('layer_shape',shape)
+        print('data range',layer_data.min(), layer_data.max())
+        min_list = np.sort(layer_data.min(-1).min(-1).min(-1))
+        max_list = np.sort(layer_data.max(-1).max(-1).max(-1))
+        abs_list = np.sort(np.abs(layer_data).max(-1).max(-1).max(-1))
+        print('n group range', min_list, max_list)
+        print('n group abs',abs_list[0],abs_list[-1])
+        print('list len', min_list.shape, abs_list.shape)
+        print('section 1/8', np.sum(abs_list<=0.125*abs_list.max()))
+        print('section 1/4', np.sum(abs_list<=0.25*abs_list.max()))
+        print('section 1/2', np.sum(abs_list<=0.5*abs_list.max()))
